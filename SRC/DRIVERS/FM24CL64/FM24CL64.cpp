440ae05146f4ba51ea294cf976fa1d06e92b4fb7
unknown <Administrator@.(none)>
2014-11-01 16:02:13 +0800

    spi ok

--- SRC/DRIVERS/FM24CL64/FM24CL64.cpp
+++ SRC/DRIVERS/FM24CL64/FM24CL64.cpp
@@ -12,9 +12,13 @@ UINT16 g_save_addr=0;
 HANDLE  hSPI;
 HANDLE  hGpio;
 #endif
+BOOL ReadReg(UINT8* data,UINT8 size);
+BOOL WriteReg(UINT8* value,UINT8 size);
+
 BOOL Virtual_Alloc()
 {
-
+	unsigned char send[3]={0x00,0x00,0x34};
+	unsigned char recv[2]={0x05,0x00};
 #ifndef USE_25115A
 	m_hI2C = I2COpen(SOCGetI2CDeviceByBus(3)); //nmcca: using I2C3_MEMBASE make sense doesnt return a good value... so nothing is opened. Using hardcoded value 3 for now     
 	I2CSetSlaveAddress(m_hI2C,  FM24CL64_I2C_DEVICE_ADDR); //nmcca: there is nothing to check to see if the handle is null...
@@ -44,6 +48,19 @@ BOOL Virtual_Alloc()
 		RETAILMSG(1, (TEXT("ERROR: PddInitializeHardware: Failed configure SPI device driver\r\n")));
 		return FALSE;
 	}
+	//RETAILMSG(1,(TEXT("Use 25115A chip\r\n")));
+	SPILockController(hSPI,INFINITE);
+	/*SPIEnableChannel(hSPI);
+	SPIWriteRead(hSPI, sizeof(UINT8)*2, recv,send);
+	SPIDisableChannel(hSPI);
+	RETAILMSG(1,(TEXT("get mode %x \r\n"),send[1]));
+	send[1]=0x00;
+	WriteReg(send,3);
+	Sleep(10);
+	g_save_addr=0x0000;
+	recv[0]=0xff;
+	ReadReg((BYTE *)recv,1);
+	RETAILMSG(1,(TEXT("get data %x \r\n"),recv[0]));*/
 #endif
 	return TRUE;
 }
@@ -84,9 +101,9 @@ BOOL FMC_Deinit(DWORD hDeviceContext)
 } 
 DWORD FMC_Init(DWORD dwContext)
 {
-	RETAILMSG(1,(TEXT("[	FM24CL64++]\r\n")));
+	RETAILMSG(1,(TEXT("[	SPI FM24CL64++]\r\n")));
 	Virtual_Alloc();
-	RETAILMSG(1,(TEXT("[	FM24CL64--]\r\n")));
+	RETAILMSG(1,(TEXT("[	SPI FM24CL64--]\r\n")));
 	return TRUE;
 }
 BOOL WriteReg(UINT8* value,UINT8 size)
@@ -106,11 +123,19 @@ BOOL WriteReg(UINT8* value,UINT8 size)
 #else
 	if(hSPI)
 	{
-		unsigned char *Local_buffer=(unsigned char *)malloc(size+1);
-		Local_buffer[0]=0x2;
-		memcpy(&(Local_buffer[1]),value,size);
-		SPIWriteRead(hSPI, sizeof(UINT8)*(size+1), Local_buffer,Local_buffer);
-		free(Local_buffer);
+		int i;
+		unsigned char *out_buffer=(unsigned char *)malloc(size+1);
+		unsigned char *in_buffer=(unsigned char *)malloc(size+1);
+		out_buffer[0]=0x2;
+		memcpy(&(out_buffer[1]),value,size);
+		/*RETAILMSG(1,(TEXT("\r\nWrite OUT>\r\n")));
+		for(i=0;i<size+1;i++)
+			RETAILMSG(1,(TEXT("%x "),out_buffer[i]));*/
+		SPIEnableChannel(hSPI);
+		SPIWriteRead(hSPI, sizeof(UINT8)*(size+1), out_buffer,in_buffer);
+		SPIDisableChannel(hSPI);
+		free(in_buffer);
+		free(out_buffer);
 	}
 	return TRUE;
 #endif
@@ -131,13 +156,25 @@ BOOL ReadReg(UINT8* data,UINT8 size)
 #else
 	if(hSPI)
 	{
-		unsigned char *Local_buffer=(unsigned char *)malloc(size+3);
-		Local_buffer[0]=0x3;
-		Local_buffer[1]=(g_save_addr)&0xff;
-		Local_buffer[2]=(g_save_addr>>8)&0xff;
-		SPIWriteRead(hSPI, sizeof(UINT8)*(size+3), Local_buffer,Local_buffer);
-		memcpy(data,&(Local_buffer[3]),size);
-		free(Local_buffer);
+		int i;
+		unsigned char *out_buffer=(unsigned char *)malloc(size+3);
+		unsigned char *in_buffer=(unsigned char *)malloc(size+3);
+		memset(out_buffer,0,size+3);
+		out_buffer[0]=0x3;
+		out_buffer[1]=(g_save_addr)&0xff;
+		out_buffer[2]=(g_save_addr>>8)&0xff;
+		/*RETAILMSG(1,(TEXT("\r\nRead OUT>\r\n")));
+		for(i=0;i<3;i++)
+			RETAILMSG(1,(TEXT("%x "),out_buffer[i]));*/
+		SPIEnableChannel(hSPI);
+		SPIWriteRead(hSPI, sizeof(UINT8)*(size+3), out_buffer,in_buffer);
+		SPIDisableChannel(hSPI);
+		/*RETAILMSG(1,(TEXT("\r\nIN<\r\n")));
+		for(i=0;i<size+3;i++)
+			RETAILMSG(1,(TEXT("%x "),in_buffer[i]));*/
+		memcpy(data,&(in_buffer[3]),size);
+		free(out_buffer);
+		free(in_buffer);
 	}
 	return TRUE;
 #endif
@@ -169,18 +206,18 @@ BOOL FMC_IOControl(DWORD hOpenContext,
 				WriteReg((BYTE *)pBufOut,dwLenOut);
 
 #endif
-			RETAILMSG(1,(TEXT("Write =\r\n")));
+			RETAILMSG(1,(TEXT("\r\nWrite =\r\n")));
 			for(i=0;i<dwLenOut;i++)
-				RETAILMSG(1,(TEXT("%x\r\n"),*(BYTE*)(pBufOut+i)));
+				RETAILMSG(1,(TEXT("%x "),*(BYTE*)(pBufOut+i)));
 			break;
 		case IOCTL_FM24CL64_READ:
 			if(pBufOut && dwLenOut>=sizeof(BYTE))
 			{
-				RETAILMSG(1,(TEXT("Read =\r\n")));
+				RETAILMSG(1,(TEXT("\r\nRead =\r\n")));
 				ReadReg((BYTE*)pBufOut,dwLenOut);
 				//RETAILMSG(1,(TEXT("Read %x="),*(BYTE*)pBufIn));
 				for(i=0;i<dwLenOut;i++)
-					RETAILMSG(1,(TEXT("%x\r\n"),*(BYTE*)(pBufOut+i)));
+					RETAILMSG(1,(TEXT("%x "),*(BYTE*)(pBufOut+i)));
 				if(pdwActualOut) *(int*)pdwActualOut=sizeof(BYTE)*dwLenOut;
 			}
 
