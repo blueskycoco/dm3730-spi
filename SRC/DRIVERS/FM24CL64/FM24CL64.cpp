717428de748f2a1ef26aee514d89b00e853199fc
unknown <Administrator@.(none)>
2014-11-02 08:22:05 +0800

    add debug macro

--- SRC/DRIVERS/FM24CL64/FM24CL64.cpp
+++ SRC/DRIVERS/FM24CL64/FM24CL64.cpp
@@ -12,12 +12,13 @@ UINT16 g_save_addr=0;
 HANDLE  hSPI;
 HANDLE  hGpio;
 #endif
+#define debug 0
 BOOL ReadReg(UINT8* data,UINT8 size);
 BOOL WriteReg(UINT8* value,UINT8 size);
 
 BOOL Virtual_Alloc()
 {
-	unsigned char send[3]={0x00,0x00,0x34};
+	unsigned char send[3]={0x00,0x00,0x67};
 	unsigned char recv[2]={0x05,0x00};
 #ifndef USE_25115A
 	m_hI2C = I2COpen(SOCGetI2CDeviceByBus(3)); //nmcca: using I2C3_MEMBASE make sense doesnt return a good value... so nothing is opened. Using hardcoded value 3 for now     
@@ -48,11 +49,13 @@ BOOL Virtual_Alloc()
 		RETAILMSG(1, (TEXT("ERROR: PddInitializeHardware: Failed configure SPI device driver\r\n")));
 		return FALSE;
 	}
+	#if debug
 	//RETAILMSG(1,(TEXT("Use 25115A chip\r\n")));
 	SPILockController(hSPI,INFINITE);
-	/*SPIEnableChannel(hSPI);
+	SPIEnableChannel(hSPI);
 	SPIWriteRead(hSPI, sizeof(UINT8)*2, recv,send);
 	SPIDisableChannel(hSPI);
+	SPIUnlockController(hSPI);
 	RETAILMSG(1,(TEXT("get mode %x \r\n"),send[1]));
 	send[1]=0x00;
 	WriteReg(send,3);
@@ -60,7 +63,8 @@ BOOL Virtual_Alloc()
 	g_save_addr=0x0000;
 	recv[0]=0xff;
 	ReadReg((BYTE *)recv,1);
-	RETAILMSG(1,(TEXT("get data %x \r\n"),recv[0]));*/
+	RETAILMSG(1,(TEXT("get data %x \r\n"),recv[0]));
+	#endif
 #endif
 	return TRUE;
 }
@@ -128,12 +132,16 @@ BOOL WriteReg(UINT8* value,UINT8 size)
 		unsigned char *in_buffer=(unsigned char *)malloc(size+1);
 		out_buffer[0]=0x2;
 		memcpy(&(out_buffer[1]),value,size);
-		/*RETAILMSG(1,(TEXT("\r\nWrite OUT>\r\n")));
+		#if debug
+		RETAILMSG(1,(TEXT("\r\nWrite OUT>\r\n")));
 		for(i=0;i<size+1;i++)
-			RETAILMSG(1,(TEXT("%x "),out_buffer[i]));*/
+			RETAILMSG(1,(TEXT("%x "),out_buffer[i]));
+		#endif
+		SPILockController(hSPI,INFINITE);
 		SPIEnableChannel(hSPI);
 		SPIWriteRead(hSPI, sizeof(UINT8)*(size+1), out_buffer,in_buffer);
 		SPIDisableChannel(hSPI);
+		SPIUnlockController(hSPI);
 		free(in_buffer);
 		free(out_buffer);
 	}
@@ -163,15 +171,21 @@ BOOL ReadReg(UINT8* data,UINT8 size)
 		out_buffer[0]=0x3;
 		out_buffer[1]=(g_save_addr)&0xff;
 		out_buffer[2]=(g_save_addr>>8)&0xff;
-		/*RETAILMSG(1,(TEXT("\r\nRead OUT>\r\n")));
+		#if debug
+		RETAILMSG(1,(TEXT("\r\nRead OUT>\r\n")));
 		for(i=0;i<3;i++)
-			RETAILMSG(1,(TEXT("%x "),out_buffer[i]));*/
+			RETAILMSG(1,(TEXT("%x "),out_buffer[i]));
+		#endif
+		SPILockController(hSPI,INFINITE);
 		SPIEnableChannel(hSPI);
 		SPIWriteRead(hSPI, sizeof(UINT8)*(size+3), out_buffer,in_buffer);
 		SPIDisableChannel(hSPI);
-		/*RETAILMSG(1,(TEXT("\r\nIN<\r\n")));
+		SPIUnlockController(hSPI);
+		#if debug
+		RETAILMSG(1,(TEXT("\r\nIN<\r\n")));
 		for(i=0;i<size+3;i++)
-			RETAILMSG(1,(TEXT("%x "),in_buffer[i]));*/
+			RETAILMSG(1,(TEXT("%x "),in_buffer[i]));
+		#endif
 		memcpy(data,&(in_buffer[3]),size);
 		free(out_buffer);
 		free(in_buffer);
@@ -206,18 +220,24 @@ BOOL FMC_IOControl(DWORD hOpenContext,
 				WriteReg((BYTE *)pBufOut,dwLenOut);
 
 #endif
+			#if debug
 			RETAILMSG(1,(TEXT("\r\nWrite =\r\n")));
 			for(i=0;i<dwLenOut;i++)
 				RETAILMSG(1,(TEXT("%x "),*(BYTE*)(pBufOut+i)));
+			#endif
 			break;
 		case IOCTL_FM24CL64_READ:
 			if(pBufOut && dwLenOut>=sizeof(BYTE))
 			{
+				#if debug
 				RETAILMSG(1,(TEXT("\r\nRead =\r\n")));
+				#endif
 				ReadReg((BYTE*)pBufOut,dwLenOut);
 				//RETAILMSG(1,(TEXT("Read %x="),*(BYTE*)pBufIn));
+				#if debug
 				for(i=0;i<dwLenOut;i++)
 					RETAILMSG(1,(TEXT("%x "),*(BYTE*)(pBufOut+i)));
+				#endif
 				if(pdwActualOut) *(int*)pdwActualOut=sizeof(BYTE)*dwLenOut;
 			}
 
